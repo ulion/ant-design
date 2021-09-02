d5eae35ab5aa187d5a21669f6badab5d88cc7873
Ulion <ulion2002@gmail.com>
2021-09-02 08:05:07 +0800

    gitpkg

---
+++ es/skeleton/Button.d.ts
@@ -0,0 +1,11 @@
+import { SkeletonElementProps } from './Element';
+export interface SkeletonButtonProps extends Omit<SkeletonElementProps, 'size'> {
+    size?: 'large' | 'small' | 'default';
+}
+declare const SkeletonButton: {
+    (props: SkeletonButtonProps): JSX.Element;
+    defaultProps: {
+        size: string;
+    };
+};
+export default SkeletonButton;
---
+++ lib/modal/ConfirmDialog.d.ts
@@ -0,0 +1,9 @@
+import { ModalFuncProps } from './Modal';
+interface ConfirmDialogProps extends ModalFuncProps {
+    afterClose?: () => void;
+    close: (...args: any[]) => void;
+    autoFocusButton?: null | 'ok' | 'cancel';
+    rootPrefixCls: string;
+}
+declare const ConfirmDialog: (props: ConfirmDialogProps) => JSX.Element;
+export default ConfirmDialog;
